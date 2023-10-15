b34a8ab6d5630bdb30f9b5cae32151e8c0b1b913
Petikov Vladimir (KROX) <developer-krox@yandex.ru>
2023-10-15 18:25:48 +0300

    151023-1830 Закончил блок преимуществ

--- src/components/advantages.jsx
+++ src/components/advantages.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import sistern from '../assets/images/sistern.png';
-import track from '../assets/images/track_1.png';
-import cards from '../assets/images/cards_1.png';
-import baloon from '../assets/images/baloon_1.png';
+import sistern from '@images/sistern.png';
+import track from '@images/track_1.png';
+import cards from '@images/cards_1.png';
+import baloon from '@images/baloon_1.png';
 
 import {device, size_screen} from './device';
 
@@ -33,46 +33,71 @@ const AdvantagesCard = styled.div`
 `;
 
 const AdvantagesCardImage = styled.img`
-   /* color: ${props => props.srcImage};  */
-   margin-top: -30px;
+   margin-top: ${(props) => (props.mtop ? props.mtop : '-30px')}; 
 `;
 
 const AdvantagesCardTitle = styled.p`
     font-family: RussoOne;
+    white-space: pre-wrap;
     font-size: 20px;
     font-weight: 400;
     line-height: 24px;
     color: var(--master-color);
-    padding: 5px 0 15px 0;
+    padding: 5px 0 0 0;
+    margin-bottom: -5px;
     text-align: center;
     margin-top: -5px;
-
 `;
+
+const AdvantagesCardSubTitle = styled.p`
+    white-space: pre-wrap;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 19px;
+    color: var(--slave-color);
+    /* color: rgba(241,241,241,.8); */
+    padding: 5px 20px;
+    text-align: center;
+    /* margin-top: -5px; */
+`;
+
+const Advante = [
+  {img: sistern,
+   title: 'Экономия\nна хранилище',
+   subtitle: 'за счёт собственных цистерн на нашей базе вы экономите до 10% вашего бюджета',
+  },
+  {img: track,
+   title: 'Экономия\nна транспорте',
+   subtitle: 'более 10 собственных газовозов которые экономят 5 – 7% ваших денежных средств'
+  },
+  {img: cards,
+   title: 'Оплата удобным\nдля вас способом',
+   subtitle: 'вы можете оплачивать как наличными, так и по терминалу который оборудован во все наши газовозы',
+  },
+  {img: baloon,
+   title: 'Любые объёмы поставки качественного газа',
+   subtitle: 'вы получаете объем от 1 до 20 тонн, с сертификатом качества газа',
+   mtop: '-72px',
+  },
+];
 
 export const AdvantagesBox = () => {
   // ========================================================================
+
   // =========================================================================
     return (
         <>               
-          <AdvantagesSection>
-            <AdvantagesCard >
-                <AdvantagesCardImage src={sistern} />
-                <AdvantagesCardTitle>Экономия на хранилище</AdvantagesCardTitle>
+         <AdvantagesSection>
+          {
+            Advante.map((item, index) => (
+              <AdvantagesCard >
+                <AdvantagesCardImage src={item.img}  mtop={item.mtop} />            
+                <AdvantagesCardTitle>{item.title}</AdvantagesCardTitle>
+                <AdvantagesCardSubTitle>{item.subtitle}</AdvantagesCardSubTitle>
             </AdvantagesCard>
-            <AdvantagesCard >
-                <AdvantagesCardImage src={track} />
-                <AdvantagesCardTitle>Экономия на транспорте</AdvantagesCardTitle>
-            </AdvantagesCard>
-            <AdvantagesCard >
-                <AdvantagesCardImage src={cards} />
-                <AdvantagesCardTitle>Оплата удобным <span>{<br/>}</span> для вас способом</AdvantagesCardTitle>
-            </AdvantagesCard>
-            <AdvantagesCard >
-                <AdvantagesCardImage src={baloon} />
-                <AdvantagesCardTitle>Любые объёмы поставки качественного газа</AdvantagesCardTitle>
-            </AdvantagesCard>
-            
-          </AdvantagesSection>         
+            ))
+          }
+         </AdvantagesSection>
         </>
       );
 
