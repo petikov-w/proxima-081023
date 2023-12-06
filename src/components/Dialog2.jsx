44c9b568d8c11f0cf1937ec85e89e5111c8cd80e
Petikov Vladimir (KROX) <developer-krox@yandex.ru>
2023-12-06 18:27:30 +0300

    061223-1830

--- src/components/Dialog2.jsx
+++ src/components/Dialog2.jsx
@@ -11,7 +11,7 @@ import DialogTitle from '@mui/material/DialogTitle';
 import { Typography } from '@mui/material';
 import axios from 'axios';
 
-import {Link } from '@mui/material';
+//import {Link } from '@mui/material';
 
 import ButtonQuestion from '../assets/images/button-question.png';
 
@@ -19,10 +19,10 @@ import ButtonQuestion from '../assets/images/button-question.png';
 
 const api = 'https://api.proxima2.kroxdev.ru';
 
-const ButtonImage = styled.img`   
-   width: 100%;
-   cursor: pointer;
-`;
+// const ButtonImage = styled.img`
+//    width: 100%;
+//    cursor: pointer;
+// `;
 
 const titleSX = {
   fontFamily: 'RussoOne', 
@@ -46,100 +46,90 @@ const buttonSX = {
 
 
 export const Dialog2 = (props) => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const {activ, setActiv, setValues} = props;
-  const [inputName, setInputName] = useState('');
-  const [inputTelefon, setInputTelefon] = useState('');
-  const [inputQuestion, setInputQuestion] = useState('');
-  
+    const {activ, setActiv, setValues} = props;
+    const [inputName, setInputName] = useState('');
+    const [inputTelefon, setInputTelefon] = useState('');
+    const [inputQuestion, setInputQuestion] = useState('');
 
-  const inputNameHandler = (e) => {
+
+    const inputNameHandler = (e) => {
         setInputName(e.target.value);
-  };
+    };
 
-  const inputTelefonHandler = (e) => {
-    setInputTelefon(e.target.value);
-  };
+    const inputTelefonHandler = (e) => {
+        setInputTelefon(e.target.value);
+    };
 
-  const inputQuestionHandler = (e) => {
-    setInputQuestion(e.target.value);
-  };
-  
-  // const submitHandler = (event) => {
-    const submitHandler = async(event) => {
-        // event.preventDefault();
-        console.log('-- name -->>> ',inputName);
-        console.log('-- telefon -->>> ',inputTelefon);
-        console.log('-- question -->>> ',inputQuestion);
+    const inputQuestionHandler = (e) => {
+        setInputQuestion(e.target.value);
+    };
 
-        let formData = new FormData();
-        formData.append('name', inputName);
-        formData.append('telefon', inputTelefon);
-        formData.append('question', inputQuestion);
-        // axios.post(`${api}`, formData, { 'Content-Type': 'multipart/form-data' })
-        // .then(function (response) {
-        //     console.log('actions --->>> ', response.data);
-        // })
-        // .catch(function (error) {
-        //     console.log('error --->>> ', error.data);
-        // });
+    const  submitHandler = async (event) => {
+            event.preventDefault();
+            console.log('-- name -->>> ', inputName);
+            console.log('-- telefon -->>> ', inputTelefon);
+            console.log('-- question -->>> ', inputQuestion);
 
+            let formData = new FormData();
+            formData.append('name', inputName);
+            formData.append('telefon', inputTelefon);
+            formData.append('question', inputQuestion);
 
-        try {
-          // make axios post request
-          const response = await axios({
-            method: 'post',
-            url: `${api}`,
-            data: formData,
-            headers: { 'Content-Type': 'multipart/form-data' },
-          });
-          console.log('== x x x ==');
-        } catch(error) {
-          console.log(error);
-        }
-        
-      };
+            try {
+                // make axios post request
+                const response = await axios({
+                    method: 'post',
+                    url: `${api}`,
+                    data: formData,
+                    headers: {'Content-Type': 'multipart/form-data'},
+                });
+                console.log('== x x x ==', response);
+            } catch (error) {
+                console.log(error);
+            }
+        };
 
-  //};
+        return (
+            <>
+                <Dialog open={activ} onClick={() => setActiv(false)}
+                        PaperProps={{sx: {borderRadius: '20px', width: '400px'}}}>
 
-       return (
-          <>
-             <Dialog open={activ}  onClick={()=>setActiv(false)} 
-             PaperProps={{ sx: {borderRadius:'20px', width: '400px'}}}>
-
-            <form action="" onSubmit={submitHandler}>
-            <div onClick={e=> e.stopPropagation()}>
-              <DialogTitle>
-                <Typography sx={titleSX}>Задайте свой вопрос</Typography>
-              </DialogTitle>
-              <DialogContent>
-                <TextField margin="dense" id="name" placeholder="ИМЯ" fullWidth sx={{ borderColor: 'red'}} 
-                InputProps={{ sx: { borderRadius:'14px' }}} onChange={inputNameHandler}
-                 />
-                <TextField margin="dense" id="telefon" placeholder="ТЕЛЕФОН"fullWidth
-                 InputProps={{ sx: { borderRadius:'14px' }}} onChange={inputTelefonHandler}/>
-                <TextField margin="dense" 
-                           id="question" 
-                           placeholder="ВОПРОС"
-                           rows={5}
-                           rowsmax={10}
-                           multiline
-                           fullWidth
-                           InputProps={{ sx: { borderRadius:'14px' }}} 
-                           onChange={inputQuestionHandler}/>
-              </DialogContent>
-              <DialogActions sx={{ width: 'auto', display: 'flex', justifyContent: 'center' }}>         
-                <Button sx={buttonSX} onClick={()=>{setActiv(false);
-                                                    navigate('/thankyou')}} type="submit"></Button>                                
-              </DialogActions>
-              </div>
-              </form>
-            </Dialog>         
-          </>       
-    );
-  };  
-
+                    <form onSubmit={submitHandler} >
+                        {/*<form action="" onSubmit={submitHandler}>*/}
+                        <div onClick={e => e.stopPropagation()}>
+                            <DialogTitle>
+                                <Typography sx={titleSX}>Задайте свой вопрос</Typography>
+                            </DialogTitle>
+                            <DialogContent>
+                                <TextField margin="dense" id="name" placeholder="ИМЯ" fullWidth sx={{borderColor: 'red'}}
+                                           InputProps={{sx: {borderRadius: '14px'}}} onChange={inputNameHandler}
+                                />
+                                <TextField margin="dense" id="telefon" placeholder="ТЕЛЕФОН" fullWidth
+                                           InputProps={{sx: {borderRadius: '14px'}}} onChange={inputTelefonHandler}/>
+                                <TextField margin="dense"
+                                           id="question"
+                                           placeholder="ВОПРОС"
+                                           rows={5}
+                                           rowsmax={10}
+                                           multiline
+                                           fullWidth
+                                           InputProps={{sx: {borderRadius: '14px'}}}
+                                           onChange={inputQuestionHandler}/>
+                            </DialogContent>
+                            <DialogActions sx={{width: 'auto', display: 'flex', justifyContent: 'center'}}>
+                                <Button type="submit" sx={buttonSX} onClick={() => {
+                                    setActiv(false);
+                                    navigate('/thankyou')
+                                }} ></Button>
+                            </DialogActions>
+                        </div>
+                    </form>
+                </Dialog>
+            </>
+        );
+    };
 
   //hashHistory.push(`/mySite/accountview?id=${account.AccountName}`)}
 
